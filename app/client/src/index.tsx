dccedaebcefd5341285c1e05b682264d20da01e4
Iris RASOLONIRINA <rim@syntralis.com>
2022-06-12 16:38:32 +0300

    Clear body margin

--- app/client/src/index.tsx
+++ app/client/src/index.tsx
@@ -19,6 +19,7 @@ root.render(
         <Route path='login' element={<Login />} />
         <Route path='/' element={<App/>} />
 
+        {/* 404 Page */}
         <Route path='*' element={<Page404/>} />
       </Routes>
     </Router>
